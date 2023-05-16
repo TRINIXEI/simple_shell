e7dfc114688685547db16cdbb5ed2655f3b8ab51
TRINIXEI <oladejiifeoluwaeunice@gmail.com>
2023-05-16 03:49:38 -0700

    update

--- shell.h
+++ shell.h
@@ -6,4 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *n);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+
 #endif
