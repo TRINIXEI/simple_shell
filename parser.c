d65a735131b3375ddd62664274563dd0d53f1c90
CharlesOkechukwu <realcharlieok@gmail.com>
2023-05-19 04:55:54 -0700

    build command path

--- parser.c
+++ parser.c
@@ -52,3 +52,39 @@ void sig_handler(int sig_int)
 	if (sig_int == SIGINT)
 		_putchar('\n');
 }
+
+/**
+ * build_path - build path of executable command
+ * @argv: argument variable
+ *
+ * Return: full path
+ */
+
+char *build_path(char **argv)
+{
+	struct stat buffer;
+	char *path, *full_path;
+	char **path_arr;
+	int i = 0, len, path_len;
+
+	path = _getenv("PATH", environ);
+	path_arr = str_brk(path, ":");
+	len = _strlen(argv[0]);
+	while (path_arr[i] != NULL)
+	{
+		path_len = _strlen(path_arr[i]);
+		full_path = malloc(sizeof(char) * (len + path_len + 2));
+		_strcpy(full_path, path_arr[i]);
+		_strcat(full_path, "/");
+		_strcat(full_path, argv[0]);
+		_strcat(full_path, "\0");
+		if (stat(full_path, &buffer) == 0)
+		{
+			free_all(path_arr);
+			return (full_path);
+		}
+		free(full_path);
+		i++;
+	}
+	return (NULL);
+}
--- shell.h
+++ shell.h
@@ -17,6 +17,7 @@ char **str_brk(char *cmd, char *delim);
 void free_all(char **ptr_arr);
 int processor(char **argv);
 void sig_handler(int sig_int);
+char *build_path(char **argv);
 
 /* environment functions */
 char *_getenv(char *name, char **env);
